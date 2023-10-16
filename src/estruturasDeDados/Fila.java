8155260f040054bcab227c0e9ba8867c96f14f01
Mariana Megumi <122192360+MarianaMegumi@users.noreply.github.com>
2023-10-16 15:36:38 -0300

    classe Fila

--- src/estruturasDeDados/Fila.java
+++ src/estruturasDeDados/Fila.java
@@ -1,5 +1,19 @@
 package estruturasDeDados;
 
 public class Fila {
-    
+     //Remove primeiro da fila e adiciona no final
+    private ListaEncadeada<T> lista;
+
+    public Fila(ListaEncadeada<T> lista) {
+        this.lista = lista;
+    }
+
+    public void adicionar (T valor){
+        this.lista.adicionar(valor);
+    }
+
+    public void remover (){
+        this.lista.remover(this.lista.getPrimeiro().getValor());
+    }
 }
+
